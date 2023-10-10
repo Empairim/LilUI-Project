9a4b1fc3a8a7842f6b1f76434be1a89d04d77d6e
Empairim <titusfaulknertull@yahoo.com>
2023-10-10 11:17:18 -0400

    the most simplest I could get it Im a bit proud honestly 8D

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-fetch(catUrl)
-  .then((res) => res.json())
-  .then((data) => console.log(data))
-  .catch((err) => console.log(err));
-
 function showTab(tabIndex) {
   // Hide all tabs
   let tabs = document.getElementsByClassName("tab");
@@ -17,6 +12,12 @@ function showTab(tabIndex) {
   let catUrl = "https://api.thecatapi.com/v1/images/search";
   fetch(catUrl)
     .then((res) => res.json())
-    .then((data) => console.log(data))
+    .then((data) => {
+      let img = document.createElement("img");
+      img.src = data[0].url;
+
+      selectedTab.innerHTML = "I PRESENT YOU A KITTYYYY";
+      selectedTab.appendChild(img);
+    })
     .catch((err) => console.log(err));
 }
